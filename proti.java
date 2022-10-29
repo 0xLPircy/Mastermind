49ec008f76e0b2e4bf62f071a329275e48efb659
L-Pircy <taveesha.agarwal@gmail.com>
2022-10-29 20:05:06 +0530

    init: java file

---
+++ proti.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class proti 
+{
+    public static void main() 
+    {
+        intro();
+        //clr
+        //menu
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt(); 
+    }
+    public static void intro() 
+    {
+        //clr
+        System.out.print("\n\n\n\n\n\n\t\t\t\t\t\t\t\tComputer Science Project");
+        System.out.print("\n\n\n\t\t\t\t\t\t\t\t\t\tMASTERMIND");
+        System.out.print("\n\n\n\t\t\t\t\t\t\t\t\t\tMade By:- Taveesha Agarwal");
+    }
+}
